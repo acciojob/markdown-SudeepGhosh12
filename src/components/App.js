e89f842a2225dba00b2c5e5480c1bbfcdfcbeefc
Sudeep Ghosh <133862916+SudeepGhosh12@users.noreply.github.com>
2025-06-03 14:40:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
-import "./App.css";
 
 function Markdown() {
   const [markdown, setMarkdown] = useState("");
